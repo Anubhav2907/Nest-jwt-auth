d6c68eb44904397fb07db1c466cdb83c1188d310
Anubhav2907 <u19ec135@eced.svnit.ac.in>
2021-07-01 14:43:56 +0530

    auth with passport-jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
-
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.userService.findOne(username);
     if (user && password === user.password) {
@@ -12,5 +15,14 @@ export class AuthService {
       return rest;
     }
     return null;
+  }
+  async login(user: any) {
+    const payload = {
+      name: user.name,
+      sub: user.id,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,6 @@ import * as session from 'express-session';
 import * as passport from 'passport';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: false,
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
-      },
-    }),
-  );
-  app.use(passport.initialize());
-  app.use(passport.session());
   await app.listen(3000);
 }
 bootstrap();
